33d3d5628e472baeaacbf7c3703dfe02322ef28c
gruvector <148240965+gruvector@users.noreply.github.com>
2024-07-20 07:22:55 +0500

    [FIX] Update secretTextInput
    Update secretTextInput

--- src/components/secretTextInput.tsx
+++ src/components/secretTextInput.tsx
@@ -18,7 +18,7 @@ export const SecretTextInput = ({
   return (
     <Switch.Group as="div" className="flex items-center">
       <input
-        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        className="block w-full rounded-md border-0 py-1.5 text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         type={inputType}
         placeholder="sk-..."
         value={value}
